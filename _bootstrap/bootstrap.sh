14a79dec8905e83fbb513b44e4c9a8758ef3e2af
= <=>
2024-04-30 21:30:12 +0000

    Begin bootstrapping concord

--- _bootstrap/bootstrap.sh
+++ _bootstrap/bootstrap.sh
@@ -7,7 +7,3 @@ python3 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip setuptools
 pip install -r requirements.txt
-if [ x"${ONTAP_PASS}" == "x" ]; then 
-    read -s -p "Password for ONTAP Clusters: " ONTAP_PASS
-fi
-export ONTAP_PASSWORD=$ONTAP_PASS
