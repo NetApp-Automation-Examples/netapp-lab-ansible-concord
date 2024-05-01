124bfa0798cbc69d7c1c36141ea89170263d4057
Johnathan Warlick <johnathw@netapp.com>
2024-05-01 12:05:03 -0500

    Update bootstrap.sh

--- _bootstrap/bootstrap.sh
+++ _bootstrap/bootstrap.sh
@@ -5,5 +5,7 @@ cd netapp-lab-ansible-concord
 dnf install python3.9 -y
 python3 -m venv venv
 source venv/bin/activate
+cd ansible
 pip install --upgrade pip setuptools
-pip install -r ansible/requirements.txt
+pip install -r requirements.txt
+ansible-galaxy role install -r roles/requirements.yml
