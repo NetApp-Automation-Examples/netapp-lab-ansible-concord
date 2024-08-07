0593314ff3c8eb3f22234ce9ec183b74563461ca
= <=>
2024-08-07 11:40:42 -0500

    pip too old bro

--- _bootstrap/bootstrap_docker_centos7.sh
+++ _bootstrap/bootstrap_docker_centos7.sh
@@ -3,10 +3,10 @@ cd /root
 git clone https://github.com/johnwarlick/netapp-lab-ansible-concord.git
 cd netapp-lab-ansible-concord
 yes | \cp -rf _bootstrap/CentOS-7-Base.repo /etc/yum.repos.d/CentOS-Base.repo
-sudo yum install python3 pip -y
+sudo yum install python3 -y
+pip3 install --upgrade pip 
 python3 -m venv venv
 source venv/bin/activate
-pip install --upgrade pip 
 pip install ansible
 ansible-galaxy role install geerlingguy.docker
 ansible-playbook bootstrap_docker.yml
