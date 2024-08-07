f6da05f01e3141f830c9449f16c3c8f0e1cffa71
= <=>
2024-08-07 11:36:58 -0500

    Doh, upgrade pip first fool

--- _bootstrap/bootstrap_docker_centos7.sh
+++ _bootstrap/bootstrap_docker_centos7.sh
@@ -6,6 +6,7 @@ yes | \cp -rf _bootstrap/CentOS-7-Base.repo /etc/yum.repos.d/CentOS-Base.repo
 sudo yum install python3 pip -y
 python3 -m venv venv
 source venv/bin/activate
+pip install --upgrade pip 
 pip install ansible
 ansible-galaxy role install geerlingguy.docker
 ansible-playbook bootstrap_docker.yml
