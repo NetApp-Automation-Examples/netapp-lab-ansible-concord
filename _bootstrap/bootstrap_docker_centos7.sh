adcc94e479342613da072e5dbf427416f4e8c700
= <=>
2024-08-07 11:51:57 -0500

    Fix pip upgrade step

--- _bootstrap/bootstrap_docker_centos7.sh
+++ _bootstrap/bootstrap_docker_centos7.sh
@@ -4,12 +4,9 @@ git clone https://github.com/johnwarlick/netapp-lab-ansible-concord.git
 cd netapp-lab-ansible-concord
 yes | \cp -rf _bootstrap/CentOS-7-Base.repo /etc/yum.repos.d/CentOS-Base.repo
 sudo yum install python3 -y
-pip3 install --upgrade pip 
 python3 -m venv venv
 source venv/bin/activate
-which python
-which pip 
-pip --version
+pip install --upgrade pip 
 pip install ansible
 ansible-galaxy role install geerlingguy.docker
 ansible-playbook bootstrap_docker.yml
